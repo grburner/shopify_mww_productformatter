12aad2f6c91eb287d6520528d1863abd902f0e43
Gary Burner <grburner@gmail.com>
2021-02-11 13:20:02 -0500

    added basic validation

--- readLines.js
+++ readLines.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
-const sender = require('./sendReq')
+const sender = require('./sendReq');
+const validate = require('./validator');
 
 const readLines = async () => {
     const fileStream = fs.createReadStream(process.argv[2]);
@@ -11,13 +12,27 @@ const readLines = async () => {
     });
 
     let skipper = true
+    let errors = []
+    let completed = []
+    let lineCount = 0
     for await (const line of rl) {
         if (skipper === true) {
             skipper = false
         } else {
-            sender(line.split(','))
+            const currentLine = line.split(',')
+            const validator = validate(currentLine, lineCount)
+            
+            if (validator === 'success') {
+                sender(currentLine)
+                completed.push(lineCount + 2)
+            } else {
+                errors.push(`${validator} '\n'`)
+            }
         }
+        lineCount++
     }
+    console.log(`There were errors with ${errors.length} products: \n ${errors}`)
+    if (completed.length > 0) {console.log(`These lines were completed: ${completed}`)} else {console.log(`No products entries were sent`)}
 } 
 
 readLines();
